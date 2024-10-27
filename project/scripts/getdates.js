b5a0dc34c0b49e5632982f83736ade85e4ba594f
st_dog210420 <lin23013@byui.edu>
2024-10-27 22:23:29 +0800

    added to my homepage

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -1,10 +1,4 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
 
 // copyright year
 const currentYear = new Date().getFullYear();
