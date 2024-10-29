299b28a2968d1744bb0525dca31006550d4ad9a0
Phoebe <hsingyinlin@gmail.com>
2024-10-30 00:18:16 +0800

    added to my homepage

--- project/scripts/filtered-products.js
+++ project/scripts/filtered-products.js
@@ -1,6 +1,19 @@
 const productCards = document.querySelector('.bag-cards');
 const h2title = document.querySelector('.h2-title');
 
+
+let products = [];
+const getProducts = async () => {
+    const response = await fetch("https://stdog210420.github.io/wdd131/project/scripts/products.json");
+    if (response.ok) {
+        products = await response.json();
+        displayProducts(products);
+        console.log(products)
+    }
+}
+getProducts();
+creatProductCard(products);
+
 function creatProductCard(filteredProducts) {
     productCards.innerHTML = "";
     filteredProducts.forEach(product => {
@@ -8,20 +21,20 @@ function creatProductCard(filteredProducts) {
         let figure = document.createElement("figure");
         // Create elements for each templeCards
         let figcaption = document.createElement("figcaption");
-        let bagName = document.createElement("h4");
+        let productName = document.createElement("h4");
         let category = document.createElement("p");
         let price = document.createElement("p");
         let amount = document.createElement("p");
         let img = document.createElement("img");
 
         // Set the content of the elements
-        productName.innerHTML = `${product.productName}`;
+        productName.innerHTML = `${product.Name}`;
         category.innerHTML = `Location: <span class="span-temple">${product.category}</span>`;
         price.innerHTML = `price:$ <span class="span-temple">${product.price}</span>`;
         amount.innerHTML = `Dedicated: <span class="span-temple">${product.quantity}</span>`;
         img.setAttribute("src", product.imageUrl);
         img.setAttribute("alt", `${product.Name} Bags image`);
-        img.setAttribute("loadding", "lazy");
+        img.setAttribute("loading", "lazy");
         // Append the temple details to the figcaption
         figcaption.appendChild(productName);
         figcaption.appendChild(category);
@@ -37,12 +50,4 @@ function creatProductCard(filteredProducts) {
     });
 }
 
-let products = [];
-const getProducts = async () => {
-    const response = await fetch("https://stdog210420.github.io/wdd131/project/scripts/products.json");
-    if (response.ok) {
-        productList = await response.json();
-        displayProducts(products);
-        console.log(products)
-    }
-}
+
